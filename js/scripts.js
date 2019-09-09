788fdb7f76872c92ec893c3f68b905f17ae33048
almarpa <alex_marco7@outlook.es>
2019-09-09 17:50:05 +0200

    Some javascript functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,16 @@
-(function(){
-  'use strict';
-  document.addEventListener('DOMContentLoaded', function(){
-    
-    
+(function () {
+    'use strict';
+    document.addEventListener('DOMContentLoaded', function () {
       
-      
-    
-    
-  });
-  
+      //Get elementById
+      /*
+      var logo = document.getElementById('logo');
+      var navegacion = document.getElementById('navegacion');
+      navegacion.style.display="none";
+      */
+
+      //GetElementByClassName
+      var navegacion = document.getElementsByClassName('navegacion');
+      console.log("Navegacion");
+    });
 })();
